fcb3fa7769aea03d48d90ef95fa9397c5e12783c
Ari Seyhun <ariseyhun@live.com.au>
2024-03-13 22:16:06 +0800

    feat: add support for lifetimes in messages

--- crates/simpl_actor_macros/src/actor.rs
+++ crates/simpl_actor_macros/src/actor.rs
@@ -1,19 +1,25 @@
+use std::borrow::Cow;
+
 use heck::{ToShoutySnekCase, ToUpperCamelCase};
-use proc_macro2::Span;
+use proc_macro2::{Span, TokenStream};
 use quote::{format_ident, quote, ToTokens};
 use syn::{
     parse::{Parse, ParseStream},
     parse_quote,
     punctuated::Punctuated,
     spanned::Spanned,
-    Field, FnArg, Ident, ImplItem, ItemImpl, Meta, ReturnType, Signature, Token, Type,
+    Field, FnArg, GenericArgument, GenericParam, Generics, Ident, ImplItem, ItemImpl, Lifetime,
+    LifetimeParam, Meta, ReturnType, Signature, Token, Type, Visibility,
 };
 
 pub struct Actor {
     item_impl: ItemImpl,
     ident: Ident,
+    actor_generics: Generics,
     actor_msg_ident: Ident,
+    actor_msg_generics: Option<TokenStream>,
     actor_ref_ident: Ident,
+    actor_ref_global_ident: Ident,
     messages: Vec<Message>,
 }
 
@@ -23,12 +29,13 @@ struct Message {
     sig: Signature,
     variant: Ident,
     fields: Punctuated<Field, Token![,]>,
+    generics: Generics,
 }
 
 impl From<(Visibility, Signature)> for Message {
     fn from((vis, sig): (Visibility, Signature)) -> Self {
         let variant = format_ident!("{}", sig.ident.to_string().to_upper_camel_case());
-        let fields = sig
+        let fields: Punctuated<Field, Token![,]> = sig
             .inputs
             .iter()
             .filter_map(|input| match input {
@@ -41,19 +48,52 @@ impl From<(Visibility, Signature)> for Message {
                     syn::Pat::Ident(pat_ident) => pat_ident.ident.clone(),
                     _ => format_ident!("__field{i}"),
                 };
-                let ty = &pat_type.ty;
+                let ty = match pat_type.ty.as_ref() {
+                    Type::Reference(ty_ref) => {
+                        let mut ty_ref = ty_ref.clone();
+                        ty_ref.lifetime = Some(Lifetime::new(
+                            &format!("'{}__{}", sig.ident, ident),
+                            Span::call_site(),
+                        ));
+                        Cow::Owned(Type::Reference(ty_ref))
+                    }
+                    ty => Cow::Borrowed(ty),
+                };
 
                 parse_quote! {
                     #ident: #ty
                 }
             })
             .collect();
+        let generics_punctuated: Punctuated<_, Token![,]> = fields
+            .iter()
+            .filter_map(|field| match &field.ty {
+                Type::Reference(ty_ref) => ty_ref
+                    .lifetime
+                    .clone()
+                    .map(|lifetime| GenericParam::Lifetime(LifetimeParam::new(lifetime))),
+                _ => None,
+            })
+            .chain(
+                sig.generics
+                    .params
+                    .iter()
+                    .filter(|param| matches!(param, GenericParam::Type(_)))
+                    .cloned(),
+            )
+            .collect();
+        let generics = if generics_punctuated.is_empty() {
+            Generics::default()
+        } else {
+            parse_quote! { < #generics_punctuated > }
+        };
 
         Message {
             vis,
             sig,
             variant,
             fields,
+            generics,
         }
     }
 }
@@ -99,6 +139,7 @@ impl Actor {
 
     fn expand_msg_enum(&self) -> proc_macro2::TokenStream {
         let Self {
+            actor_generics,
             actor_msg_ident,
             messages,
             ..
@@ -109,6 +150,7 @@ impl Actor {
                  sig,
                  variant,
                  fields,
+                 ..
              }| {
                 let ret_ty: Type = match &sig.output {
                     ReturnType::Default => parse_quote! { () },
@@ -125,7 +167,7 @@ impl Actor {
         );
 
         quote! {
-            enum #actor_msg_ident {
+            enum #actor_msg_ident #actor_generics {
                 #( #variants ),*
             }
         }
@@ -243,6 +285,7 @@ impl Actor {
     fn expand_actor_ref_struct(&self) -> proc_macro2::TokenStream {
         let Self {
             actor_msg_ident,
+            actor_msg_generics,
             actor_ref_ident,
             ..
         } = self;
@@ -250,7 +293,7 @@ impl Actor {
         quote! {
             #[derive(Clone, Debug)]
             pub struct #actor_ref_ident {
-                channel: ::tokio::sync::mpsc::Sender<::simpl_actor::Signal<#actor_msg_ident>>,
+                channel: ::tokio::sync::mpsc::Sender<::simpl_actor::Signal<#actor_msg_ident #actor_msg_generics>>,
                 stop_notify: ::std::sync::Arc<::tokio::sync::Notify>
             }
         }
@@ -260,26 +303,58 @@ impl Actor {
         let Self {
             actor_msg_ident,
             actor_ref_ident,
+            actor_ref_global_ident,
             messages,
             ..
         } = self;
 
-        let methods = messages.iter().map(
-            |Message {
+        let methods = messages.iter().enumerate().map(
+            |(i, Message {
                  vis,
                  sig,
                  variant,
                  fields,
-             }| {
+                 generics,
+             })| {
                  let field_idents: Vec<_> = fields
                      .iter()
                      .map(|field| field.ident.as_ref().unwrap())
                      .collect();
                 let field_tys: Punctuated<_, Token![,]> = fields.iter().map(|field| &field.ty).collect();
 
+                let channel = {
+                    let generic_params: Punctuated<_, Token![,]> = messages.iter().enumerate().flat_map(|(j, Message { generics, .. })| {
+                        generics
+                            .lifetimes()
+                            .map(move |lt| {
+                                if i == j {
+                                    GenericArgument::Lifetime(lt.lifetime.clone())
+                                } else {
+                                    GenericArgument::Lifetime(Lifetime::new("'static", Span::call_site()))
+                                }
+                            })
+                            .chain(generics.type_params().map(move |tp| {
+                                if i == j {
+                                    let ident = &tp.ident;
+                                    GenericArgument::Type(parse_quote! { #ident })
+                                } else {
+                                    GenericArgument::Type(parse_quote! { () })
+                                }
+
+                            }))
+                    }).collect();
+                    let all_generics = (!generic_params.is_empty()).then_some(quote! { < #generic_params > });
+                    quote! {{
+                        let channel: &::tokio::sync::mpsc::Sender<::simpl_actor::Signal<CounterActorMsg #all_generics>> =
+                            unsafe { ::std::mem::transmute(&self.channel) };
+                        channel
+                    }}
+                };
+
                 let mut sig = sig.clone();
                 sig.constness = None;
                 sig.asyncness = Some(Token![async](Span::call_site()));
+                sig.generics = generics.clone();
                 sig.inputs = [parse_quote! { &self }]
                     .into_iter()
                     .chain(
@@ -335,16 +410,17 @@ impl Actor {
                     "cannot call non-async messages on self as this would deadlock - please use the {} variant instead\nthis assertion only occurs on debug builds, release builds will deadlock",
                     try_async_sig.ident
                 );
+
                 quote! {
-                    pub #sig {
+                    #[allow(non_snake_case)]
                     #vis #sig {
                         debug_assert!(
                             #actor_ref_global_ident.try_with(|_| {}).is_err(),
                             #normal_debug_msg
                         );
+
                         let (reply, rx) = ::tokio::sync::oneshot::channel();
-                        self
-                            .channel
+                        #channel
                             .send(::simpl_actor::Signal::Message(#actor_msg_ident::#variant {
                                 __reply: ::std::option::Option::Some(reply),
                                 #( #field_idents ),*
@@ -355,15 +431,15 @@ impl Actor {
                         rx.await.map_err(|_| ::simpl_actor::ActorError::ActorStopped)
                     }
 
-                    pub #timeout_sig {
+                    #[allow(non_snake_case)]
                     #vis #timeout_sig {
                         debug_assert!(
                             #actor_ref_global_ident.try_with(|_| {}).is_err(),
                             #timeout_debug_msg
                         );
+
                         let (reply, rx) = ::tokio::sync::oneshot::channel();
-                        self
-                            .channel
+                        #channel
                             .send_timeout(
                                 ::simpl_actor::Signal::Message(#actor_msg_ident::#variant {
                                     __reply: ::std::option::Option::Some(reply),
@@ -377,7 +453,7 @@ impl Actor {
                         rx.await.map_err(|_| ::simpl_actor::ActorError::ActorStopped)
                     }
 
-                    pub #try_sig {
+                    #[allow(non_snake_case)]
                     #vis #try_sig {
                         debug_assert!(
                             #actor_ref_global_ident.try_with(|_| {}).is_err(),
@@ -385,8 +461,7 @@ impl Actor {
                         );
 
                         let (reply, rx) = ::tokio::sync::oneshot::channel();
-                        self
-                            .channel
+                        #channel
                             .try_send(::simpl_actor::Signal::Message(#actor_msg_ident::#variant {
                                 __reply: ::std::option::Option::Some(reply),
                                 #( #field_idents ),*
@@ -396,10 +471,9 @@ impl Actor {
                         rx.await.map_err(|_| ::simpl_actor::ActorError::ActorStopped)
                     }
 
-                    pub #async_sig {
-                        self
-                            .channel
+                    #[allow(non_snake_case)]
                     #vis #async_sig {
+                        #channel
                             .send(::simpl_actor::Signal::Message(#actor_msg_ident::#variant {
                                 __reply: ::std::option::Option::None,
                                 #( #field_idents ),*
@@ -410,10 +484,9 @@ impl Actor {
                         Ok(())
                     }
 
-                    pub #async_timeout_sig {
-                        self
-                            .channel
+                    #[allow(non_snake_case)]
                     #vis #async_timeout_sig {
+                        #channel
                             .send_timeout(
                                 ::simpl_actor::Signal::Message(#actor_msg_ident::#variant {
                                     __reply: ::std::option::Option::None,
@@ -427,10 +500,9 @@ impl Actor {
                         Ok(())
                     }
 
-                    pub #try_async_sig {
-                        self
-                            .channel
+                    #[allow(non_snake_case)]
                     #vis #try_async_sig {
+                        #channel
                             .try_send(::simpl_actor::Signal::Message(#actor_msg_ident::#variant {
                                 __reply: ::std::option::Option::None,
                                 #( #field_idents ),*
@@ -485,7 +557,9 @@ impl Actor {
         let Self {
             ident,
             actor_msg_ident,
+            actor_msg_generics,
             actor_ref_ident,
+            actor_ref_global_ident,
             messages,
             ..
         } = self;
@@ -495,6 +569,7 @@ impl Actor {
                  sig,
                  variant,
                  fields,
+                 ..
              }| {
                 let fn_ident = &sig.ident;
                 let dot_await = if sig.asyncness.is_some() {
@@ -521,13 +596,10 @@ impl Actor {
             },
         );
 
-        let task_local_ident =
-            format_ident!("{}_ACTOR_REF", ident.to_string().TO_SHOUTY_SNEK_CASE());
-
         quote! {
             ::tokio::task_local! {
                 #[doc(hidden)]
-                static #task_local_ident: #actor_ref_ident;
+                static #actor_ref_global_ident: #actor_ref_ident;
             }
 
             #[automatically_derived]
@@ -537,7 +609,7 @@ impl Actor {
                 fn spawn(self) -> Self::Ref {
                     fn handle_messages<'a>(
                         actor: &'a mut #ident,
-                        rx: &'a mut ::tokio::sync::mpsc::Receiver<::simpl_actor::Signal<#actor_msg_ident>>,
+                        rx: &'a mut ::tokio::sync::mpsc::Receiver<::simpl_actor::Signal<#actor_msg_ident #actor_msg_generics>>,
                     ) -> ::core::pin::Pin<::std::boxed::Box<dyn ::core::future::Future<Output = ()> + ::core::marker::Send + 'a>> {
                         ::std::boxed::Box::pin(async move {
                             while let ::std::option::Option::Some(signal) = rx.recv().await {
@@ -561,8 +633,8 @@ impl Actor {
                         stop_notify: ::std::sync::Arc::clone(&stop_notify)
                     };
 
-                    ::tokio::spawn(#task_local_ident.scope(actor_ref.clone(), async move {
-                        ::simpl_actor::run_actor_lifecycle::<Self, #actor_msg_ident, _>(
+                    ::tokio::spawn(#actor_ref_global_ident.scope(actor_ref.clone(), async move {
+                        ::simpl_actor::run_actor_lifecycle::<Self, #actor_msg_ident #actor_msg_generics, _>(
                             self,
                             rx,
                             stop_notify,
@@ -573,8 +645,8 @@ impl Actor {
                     actor_ref
                 }
 
-                fn actor_ref() -> Option<Self::Ref> {
-                    #task_local_ident.try_with(|actor_ref| actor_ref.clone()).ok()
+                fn try_actor_ref() -> Option<Self::Ref> {
+                    #actor_ref_global_ident.try_with(|actor_ref| actor_ref.clone()).ok()
                 }
             }
         }
@@ -624,13 +696,60 @@ impl Parse for Actor {
         };
         let actor_msg_ident = format_ident!("{ident}Msg");
         let actor_ref_ident = format_ident!("{ident}Ref");
+        let actor_ref_global_ident =
+            format_ident!("{}_ACTOR_REF", ident.to_string().TO_SHOUTY_SNEK_CASE());
         let messages = Actor::extract_messages(&mut item_impl);
+        let actor_generic_params: Punctuated<_, Token![,]> = messages
+            .iter()
+            .flat_map(|Message { sig, fields, .. }| {
+                fields
+                    .iter()
+                    .filter_map(|field| match &field.ty {
+                        Type::Reference(ty_ref) => ty_ref
+                            .lifetime
+                            .clone()
+                            .map(|lifetime| GenericParam::Lifetime(LifetimeParam::new(lifetime))),
+                        _ => None,
+                    })
+                    .chain(sig.generics.params.iter().filter_map(|param| match param {
+                        GenericParam::Type(ty_param) => {
+                            let mut ty_param = ty_param.clone();
+                            ty_param.bounds = Punctuated::new();
+                            Some(GenericParam::Type(ty_param))
+                        }
+                        _ => None,
+                    }))
+            })
+            .collect();
+        let has_generic_params = !actor_generic_params.is_empty();
+        let actor_generics = Generics {
+            lt_token: has_generic_params.then_some(Token![<](Span::call_site())),
+            params: actor_generic_params,
+            gt_token: has_generic_params.then_some(Token![>](Span::call_site())),
+            where_clause: None,
+        };
+        let actor_msg_generics =
+            {
+                let mut generic_arguments: Punctuated<_, Token![,]> = Punctuated::new();
+                generic_arguments.extend(actor_generics.lifetimes().map(|_| {
+                    GenericArgument::Lifetime(Lifetime::new("'static", Span::call_site()))
+                }));
+                generic_arguments.extend(
+                    actor_generics
+                        .type_params()
+                        .map(|_| GenericArgument::Type(Type::Tuple(parse_quote! { () }))),
+                );
+                (!generic_arguments.is_empty()).then_some(quote! { < #generic_arguments > })
+            };
 
         Ok(Actor {
             item_impl,
             ident,
+            actor_generics,
             actor_msg_ident,
+            actor_msg_generics,
             actor_ref_ident,
+            actor_ref_global_ident,
             messages,
         })
     }
