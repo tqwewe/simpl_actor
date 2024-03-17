6364c713af3d4b8fbb274f9d680a838ea94c9ec9
Ari Seyhun <ariseyhun@live.com.au>
2024-03-17 22:24:23 +0800

    feat: impl `Actor::name` in derive macro

--- crates/simpl_actor_macros/src/derive_actor.rs
+++ crates/simpl_actor_macros/src/derive_actor.rs
@@ -12,11 +12,16 @@ impl ToTokens for DeriveActor {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
         let ident = &self.ident;
         let actor_ref_ident = format_ident!("{ident}Ref");
+        let name = ident.to_string();
 
         tokens.extend(quote! {
             #[automatically_derived]
             impl ::simpl_actor::Actor for #ident {
                 type Ref = #actor_ref_ident;
+
+                fn name(&self) -> ::std::borrow::Cow<'_, str> {
+                    ::std::borrow::Cow::Borrowed(#name)
+                }
             }
         });
     }
